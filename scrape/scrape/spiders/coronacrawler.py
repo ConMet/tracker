11b20e728fe3a218f5215e4febd39aa8080ea92f
ConMet <46434722+ConMet@users.noreply.github.com>
2020-04-21 12:01:27 -0400

    Undoing previous commit; using BeautifulSoup

--- scrape/scrape/spiders/coronacrawler.py
+++ scrape/scrape/spiders/coronacrawler.py
@@ -6,18 +6,21 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class CoronacrawlerSpider(CrawlSpider):
     name = 'coronacrawler'
-    allowed_domains = [
-        'https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html']
-    start_urls = [
-        'http://https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html/']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'),
-             callback='parse_item', follow=True),
-    )
+    def __init__(self, *args, **kwargs):
+        self.url = kwargs.get('url')
+        self.domain = kwargs.get('domain')
+        self.start_urls = [self.url]
+        self.allowed_domains = [self.domain]
+
+        CoronacrawlerSpider.rules = [
+            Rule(LinkExtractor(unique=True), callback='parse_item'),
+        ]
+        super(CoronacrawlerSpider, self).__init__(*args, **kwargs)
 
     def parse_item(self, response):
         item = {}
+        item['url'] = response.url
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
