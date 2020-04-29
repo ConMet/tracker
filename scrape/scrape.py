5d01bec7edaabec397f49b3af24d22959e26d062
ConMet <46434722+ConMet@users.noreply.github.com>
2020-04-28 20:12:42 -0400

    Turn scrape into class, removed panda_data, integrated with scrape class. Made to_db.py for testing

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,44 +1,71 @@
 import requests
 import re
 import datetime
+import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def scrape():
-    # Collect and parse page, make soup object.
-    page_url = 'https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html'
-    page = requests.get(page_url)
-    soup = BeautifulSoup(page.text, 'html.parser')
+class Scrape():
 
-    # Sift through soup object for proper data containing div.
-    # Loop through our new soup object for table contents.
-    # Regex for cleaning out unnecessary spacing.
-    soup2 = soup.find('div', {'class': 'fullContent'})
-    for item in soup2.findAll('tbody'):
-        data = re.sub(r'\n\s*\n', r'\n\n', item.get_text().strip(), flags=re.M)
+    def __init__(self, date, county, cases, deaths):
+        self.date = date
+        self.county = county
+        self.cases = cases
+        self.deaths = deaths
 
-    # Regex for all table data placed in an ordered list:
-    # Removing spaces (ex. St Clair --> StClair) due to string method difficulty in future loop.
-    dataList = re.findall(r"^\w*.+[a-zA-Z+]|\d+", data, re.MULTILINE)
-    newDataList = [y.replace(' ', '') for y in dataList]
+    def scrapedata():
+        # Collect and parse page, make soup object.
+        page_url = 'https://www.michigan.gov/coronavirus/0,9753,7-406-98163_98173---,00.html'
+        page = requests.get(page_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    date, county, cases, deaths = [], [], [], []
-    # Create index alongside the listed data created above.
-    # Loop through and populate lists corresponding to the column they belong in.
-    for index, x in enumerate(newDataList):
-        if x.isalpha():
-            county.append(x)
-            if newDataList[index-1].isnumeric() and newDataList[index-2].isalpha():
-                deaths.append('0')
-        if x.isnumeric():
-            if newDataList[index-1].isnumeric():
-                deaths.append(x)
-            elif newDataList[index-1].isalpha():
-                cases.append(x)
+        # Sift through soup object for proper data containing div.
+        # Loop through our new soup object for table contents.
+        # Regex for cleaning out unnecessary spacing.
+        soup2 = soup.find('div', {'class': 'fullContent'})
+        for item in soup2.findAll('tbody'):
+            data = re.sub(r'\n\s*\n', r'\n\n',
+                          item.get_text().strip(), flags=re.M)
 
-    # Loop for populating date column with date the data was scraped.
-    dateobj = datetime.datetime.now()
-    for z in county:
-        date.append(dateobj.strftime('%m/%d/%Y'))
+        # Regex for all table data placed in an ordered list:
+        # Removing spaces (ex. St Clair --> StClair) due to string method difficulty in future loop.
+        dataList = re.findall(r"^\w*.+[a-zA-Z+]|\d+", data, re.MULTILINE)
+        newDataList = [y.replace(' ', '') for y in dataList]
 
-    return date, county, cases, deaths
+        date, county, cases, deaths = [], [], [], []
+        # Create index alongside the listed data created above.
+        # Loop through and populate lists corresponding to the column they belong in.
+        for index, x in enumerate(newDataList):
+            if x.isalpha():
+                county.append(x)
+                if newDataList[index-1].isnumeric() and newDataList[index-2].isalpha():
+                    deaths.append('0')
+            if x.isnumeric():
+                if newDataList[index-1].isnumeric():
+                    deaths.append(x)
+                elif newDataList[index-1].isalpha():
+                    cases.append(x)
+
+        # Loop for populating date column with date the data was scraped.
+        dateobj = datetime.datetime.now()
+        for z in county:
+            date.append(dateobj.strftime('%m/%d/%Y'))
+
+        return date, county, cases, deaths
+
+    def pandadata():
+        date, county, cases, deaths = Scrape.scrapedata()
+        # Create data frame
+        df = pd.DataFrame(
+            {'Date': date,
+             'Counties': county,
+             'Cases': cases,
+             'Deaths': deaths
+             })
+        return df
+
+
+# Scrape.scrapedata()[0])    - Returns Date column
+# Scrape.scrapedata()[1])    - Returns Counties column
+# Scrape.scrapedata()[2])    - Returns Cases column
+# Scrape.scrapedata()[3])    - Returns Deaths column
---
+++ scrape/to_db.py
@@ -0,0 +1,9 @@
+from scrape import *
+
+Scrape.scrapedata()
+
+
+print(scrapedata[1])
+
+# def save_to_db(dataframe):
+# Scrape.pandadata().save()
